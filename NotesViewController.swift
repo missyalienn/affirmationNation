409a1674725e2288f3e107c330b073fd844ce776
missyalienn <allan.missy@gmail.com>
2017-02-27 22:36:32 -0500

    modified json, notes vc, and tab bar

--- NotesViewController.swift
+++ NotesViewController.swift
@@ -1,42 +1,76 @@
 //
 //  NotesViewController.swift
-//  
+//  AffirmationCards
 //
-//  Created by Missy Allan on 2/24/17.
-//
+//  Created by Missy Allan on 2/27/17.
+//  Copyright © 2017 Missy Allan. All rights reserved.
 //
 
 import UIKit
 
 class NotesViewController: UIViewController {
 
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     /*
     // MARK: - Navigation
 
